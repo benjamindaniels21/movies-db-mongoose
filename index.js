0fe5b71ecdc774dc59770e5c67298ba00522011a
Ben Daniels <benjamindaniels21@gmail.com>
2023-11-02 08:02:49 -0400

    adding movie data

--- index.js
+++ index.js
@@ -25,3 +25,11 @@ const amadeus = new Movie({
   score: 9.2,
   rating: "R",
 });
+
+Movie.insertMany([
+  { title: "Amelie", year: 2001, score: 8.3, rating: "R" },
+  { title: "Alien", year: 1979, score: 8.1, rating: "R" },
+  { title: "The Iron Giant", year: 1999, score: 7.5, rating: "PG" },
+  { title: "Stand By Me", year: 1986, score: 8.6, rating: "R" },
+  { title: "Moonrise Kingdom", year: 2012, score: 7.3, rating: "PG-13" },
+]);
